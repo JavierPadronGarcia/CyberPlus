245607ffef719e1f258e9cd1a254daf2d51efa8c
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2023-10-11 15:03:27 +0100

    first commit - menu and searcher styles

---
+++ CyberPlus/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './components/menu/search/search.component';
+import { UserComponent } from './components/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SearchComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ CyberPlus/src/app/components/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger('moveDown', [
+      state('void', style({ transform: 'translateY(-100%)' })),
+      state('*', style({ transform: 'translateY(0)' })),
+      transition('void <=> *', animate('300ms ease-in-out')),
+    ])
+  ]
+})
+export class MenuComponent {
+
+  menuVisbility: boolean = false;
+  searcherVisibility: boolean = false;
+
+  changeMenuVisibility() {
+    if (this.searcherVisibility) { this.changeSearcherVisibility(); }
+    this.menuVisbility = !this.menuVisbility
+  }
+
+  changeSearcherVisibility() {
+    if (this.menuVisbility) { this.changeMenuVisibility(); }
+    this.searcherVisibility = !this.searcherVisibility
+  }
+
+}
